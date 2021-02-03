22d9416f0d869ff44cf6cafad5cf8ad4fa7eaa45
Najibkassab <najeebkassab@hotmail.com>
2021-02-03 12:06:40 +0200

    Revert "update year and verion to V0.2.4"
    This reverts commit 2cc69982410912055fcc13237cba853e74df9852.

--- VL53L0X_1.0.2/Api/platform/src/vl53l0x_platform.c
+++ VL53L0X_1.0.2/Api/platform/src/vl53l0x_platform.c
@@ -36,7 +36,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 /*
-		MODIFIED by Hexabitz for BitzOS (BOS) V0.2.4 - Copyright (C) 2017-2021 Hexabitz
+		MODIFIED by Hexabitz for BitzOS (BOS) V0.2.2 - Copyright (C) 2017-2020 Hexabitz
     All rights reserved
 */
 
