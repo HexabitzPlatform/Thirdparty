3aa9f953f479802cf02c200855f32a4d395e0b61
HALA-MUMMO\Hala MUMMO <halashem13@gmail.com>
2024-02-26 13:42:29 +0300

    - Edited release number in headers from V0.3.0 to V0.3.1

--- VL53L0X_1.0.2/Api/platform/inc/vl53l0x_i2c_platform.h
+++ VL53L0X_1.0.2/Api/platform/inc/vl53l0x_i2c_platform.h
@@ -1,5 +1,5 @@
 /*
- BitzOS (BOS) V0.3.0 - Copyright (C) 2017-2024 Hexabitz
+ BitzOS (BOS) V0.3.1 - Copyright (C) 2017-2024 Hexabitz
  All rights reserved
  
  File Name     : VL53L0X_i2c_platform.h
