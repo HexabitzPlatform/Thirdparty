a2ed759bd6f8b4f42dcf9db83dd6497f8a214e9d
Bakry Rihawi <bakry29bbb@gmail.com>
2025-06-03 13:27:50 +0300

    Adding  __WEAK to the HAL_MspInit function because the function needs the internal reference voltage of the ADC in File H0FR7_adc.c

--- STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_msp_template.c
+++ STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_msp_template.c
@@ -45,7 +45,7 @@
   * @brief  Initialize the Global MSP.
   * @retval None
   */
-void HAL_MspInit(void)
+__WEAK void HAL_MspInit(void)
 {
   /* NOTE : This function is generated automatically by STM32CubeMX and eventually
             modified by the user
